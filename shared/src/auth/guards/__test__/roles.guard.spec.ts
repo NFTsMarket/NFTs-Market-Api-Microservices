e2aaeea26f3205f2e7e49ede4beafb96c57cbfee
bujosa <davidbujosa@gmail.com>
2021-11-20 20:52:05 +0100

    Add jwt guards and test this guards

---
+++ shared/src/auth/guards/__test__/roles.guard.spec.ts
@@ -0,0 +1,141 @@
+import { BadImplementationError } from "@shared/errors/common/bad-implementation.error";
+import { UnauthorizedRoleError } from "@shared/errors/common/unauthorized-role.error";
+import { UserRoles } from "../../enums/user-roles.enum";
+import { RolesGuard } from "../roles.guard";
+
+describe("RolesGuard", () => {
+  let rolesGuard: RolesGuard;
+
+  const reflector = {
+    get: jest.fn(),
+    getAll: jest.fn(),
+    getAllAndMerge: jest.fn(),
+    getAllAndOverride: jest.fn(),
+  };
+
+  const getRequest = jest.fn();
+
+  const context: any = {
+    getType: jest.fn().mockReturnValue("http"),
+    getHandler: () => undefined,
+    switchToHttp: () => ({
+      getRequest,
+    }),
+  };
+
+  const correctTestCases = [
+    [
+      [UserRoles.CLIENT, UserRoles.CUSTOMER_CARE],
+      [
+        UserRoles.ADMIN,
+        UserRoles.MARKETING,
+        UserRoles.TECHNICIAN,
+        UserRoles.CLIENT,
+      ],
+    ],
+    [
+      [UserRoles.CUSTOMER_CARE, UserRoles.CLIENT],
+      [
+        UserRoles.CLIENT,
+        UserRoles.ADMIN,
+        UserRoles.MARKETING,
+        UserRoles.TECHNICIAN,
+        UserRoles.CUSTOMER_CARE,
+      ],
+    ],
+    [
+      [UserRoles.TECHNICIAN_SUPERVISOR, UserRoles.UPLOADER],
+      [UserRoles.TECHNICIAN_SUPERVISOR, UserRoles.ADMIN],
+    ],
+    [[UserRoles.MARKETING], [UserRoles.MARKETING]],
+    [[UserRoles.TECHNICIAN], [UserRoles.UPLOADER, UserRoles.TECHNICIAN]],
+  ];
+
+  const failedTestCases = [
+    [
+      [UserRoles.CLIENT, UserRoles.CUSTOMER_CARE],
+      [UserRoles.ADMIN, UserRoles.MARKETING, UserRoles.TECHNICIAN],
+    ],
+    [
+      [UserRoles.CUSTOMER_CARE],
+      [
+        UserRoles.CLIENT,
+        UserRoles.ADMIN,
+        UserRoles.MARKETING,
+        UserRoles.TECHNICIAN,
+      ],
+    ],
+    [[UserRoles.TECHNICIAN_SUPERVISOR, UserRoles.UPLOADER], [UserRoles.ADMIN]],
+    [[], [UserRoles.MARKETING]],
+    [[UserRoles.TECHNICIAN], []],
+  ];
+
+  beforeAll(() => {
+    rolesGuard = new RolesGuard(reflector);
+  });
+
+  describe("canActivate", () => {
+    it("should return true if authorized roles are not provided", () => {
+      //Arrange
+      reflector.get.mockReturnValueOnce(undefined);
+
+      //Act
+      const result = rolesGuard.canActivate(context);
+
+      //Assert
+      expect(result).toBe(true);
+    });
+
+    it("should return a BadImplementationError if there is no user defined in the request object", () => {
+      //Arrange
+      reflector.get.mockReturnValueOnce([]);
+      getRequest.mockReturnValueOnce({});
+
+      //Act
+      const result = () => rolesGuard.canActivate(context);
+
+      //Assert
+      expect(result).toThrow(BadImplementationError);
+    });
+
+    it.each(correctTestCases)(
+      "should return true if the user roles match at least one the authorized roles",
+      (userRoles: UserRoles[], authorizedRoles: UserRoles[]) => {
+        //Arrange
+        reflector.get.mockReturnValueOnce(authorizedRoles);
+
+        getRequest.mockReturnValueOnce({
+          user: {
+            roles: userRoles,
+          },
+        });
+
+        //Act
+        const result = rolesGuard.canActivate(context);
+
+        //Assert
+        expect(result).toBe(true);
+      }
+    );
+
+    it.each(failedTestCases)(
+      "should return a UnauthorizedRoleError none of the user roles match one of the authorized roles",
+      (userRoles: UserRoles[], authorizedRoles: UserRoles[]) => {
+        //Arrange
+        reflector.get.mockReturnValueOnce(authorizedRoles);
+
+        getRequest.mockReturnValueOnce({
+          user: {
+            roles: userRoles,
+          },
+        });
+
+        //Act
+        const result = () => rolesGuard.canActivate(context);
+
+        //Assert
+        expect(result).toThrow(UnauthorizedRoleError);
+      }
+    );
+  });
+});
