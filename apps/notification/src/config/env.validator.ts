99fe60acdb0bfc44b45945db7d4410224560cb3d
bujosa <davidbujosa@gmail.com>
2021-11-25 03:46:35 +0100

    Fixed env validator

--- apps/notification/src/config/env.validator.ts
+++ apps/notification/src/config/env.validator.ts
@@ -1,14 +1,17 @@
-import { commonEnvValidationObject } from '@shared/validations/common/env/common-env.validation-object';
 import { validateUrlWithJoi } from '@shared/validations/common/internet/url/url.validator';
 import { validateEnviromentWithJoi } from '@shared/validations/enviroment/enviroment.validator';
 import * as joi from 'joi';
 
 export const validateEnv = joi.object({
-  ...commonEnvValidationObject,
   HOST_CONFIRM_EMAIL: validateUrlWithJoi.required(),
   HOST_RESET_PASSWORD: validateUrlWithJoi.required(),
   NFTS_EMAIL: joi.string().required(),
   SENGRID_USER: joi.string().required(),
   SENGRID_API_KEY: joi.string().required(),
   MAIL_ENVIRONMENT: validateEnviromentWithJoi,
+  PORT: joi.number().default(3000),
+  GOOGLE_API_JSON_PATH: joi.string(),
+  GOOGLE_PROJECT_ID: joi.string().required(),
+  PUB_SUB_API_ENDPOINT: joi.string().required(),
+  PUB_SUB_PROJECT_ID: joi.string().required(),
 });
