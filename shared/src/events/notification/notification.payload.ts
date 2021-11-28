393ff69eda4f335cb72f936e77dbb0db06f2d600
bujosa <davidbujosa@gmail.com>
2021-11-28 02:15:32 +0100

    Eliminated some payloads

--- shared/src/events/notification/notification.payload.ts
+++ shared/src/events/notification/notification.payload.ts
@@ -1,19 +1,10 @@
 export class WelcomeUserPayload {
   name: string;
-  lastName: string;
   email: string;
 }
 
 export class ConfirmUserAccountPayload {
   name: string;
-  lastName: string;
-  email: string;
-  url: string;
-}
-
-export class CreatedUserNotification {
-  name: string;
-  lastName: string;
   email: string;
   url: string;
 }
@@ -21,10 +12,4 @@ export class CreatedUserNotification {
 export class ResetUserPasswordPayload {
   email: string;
   url: string;
-}
-
-export class UpdatedUserPasswordPayload {
-  name: string;
-  lastName: string;
-  email: string;
 }
