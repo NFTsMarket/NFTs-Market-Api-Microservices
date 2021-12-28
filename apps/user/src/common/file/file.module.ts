68be6be9f530c67fb7af618fb1fc5a20c03a5bf6
bujosa <davidbujosa@gmail.com>
2021-12-28 13:12:43 +0100

    Eliminated unused import on file module

--- apps/user/src/common/file/file.module.ts
+++ apps/user/src/common/file/file.module.ts
@@ -1,18 +1,16 @@
 import { Module } from '@nestjs/common';
 import { JwtStrategy } from '@shared/auth/strategies/jwt.strategy';
 import { GlobalJwtAuthAndRolesGuard } from '@shared/auth/guards/global-jwt-auth-and-roles.guard';
-import { Upload } from './graphql/types/upload.scalar';
 import { FileService } from './file.service';
 import { FileResolver } from './file.resolver';
 
 @Module({
   imports: [],
   providers: [
-    Upload,
+    FileService,
+    FileResolver,
     JwtStrategy,
     ...GlobalJwtAuthAndRolesGuard,
-    FileService,
-    FileResolver
   ],
 })
 export class FileModule {}
