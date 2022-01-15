317f775ab94a5ef8faf909e781aaa4cbba0e7924
bujosa <davidbujosa@gmail.com>
2022-01-16 00:16:41 +0100

    Changes generate link logic

--- apps/notification/src/notification/utils/generate-link.ts
+++ apps/notification/src/notification/utils/generate-link.ts
@@ -1,8 +1,12 @@
 import { EmailTypes } from '@shared/data/enums/email-types.enum';
 
-export const generateLink = (token: string, type: string): string => {
-  if (type === EmailTypes.CONFIRM_ACCOUNT) {
-    return `${process.env.HOST_CONFIRM_EMAIL}${token}`;
+export const generateLink = (token: string, type: EmailTypes): string => {
+  switch (type) {
+    case EmailTypes.RESET_PASSWORD:
+      return `${process.env.HOST_RESET_PASSWORD}${token}`;
+    case EmailTypes.CONFIRM_ACCOUNT:
+      return `${process.env.HOST_CONFIRM_EMAIL}${token}`;
+    default:
+      return '';
   }
-  return `${process.env.HOST_RESET_PASSWORD}${token}`;
 };
