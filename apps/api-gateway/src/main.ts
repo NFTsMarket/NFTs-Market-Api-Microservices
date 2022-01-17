6ff5b0392cbff7532a306ae90e017e446f4a681a
bujosa <davidbujosa@gmail.com>
2022-01-17 03:12:16 +0100

    Add proxy middleware

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,15 +1,66 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { graphqlUploadExpress } from 'graphql-upload';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import { AppModule } from './app.module';
 import { EnvKey } from './config/env-key.enum';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(graphqlUploadExpress({ maxFileSize: 1000000000, maxFiles: 10 }));
-
   const configService: ConfigService = app.get(ConfigService);
+
+  app.enableCors();
+
+  app.use(
+    '/wallet',
+    createProxyMiddleware({
+      target: configService.get(EnvKey.WALLET_SERVICE),
+      changeOrigin: true,
+      pathRewrite: {
+        '^/wallet': '',
+      },
+    }),
+  );
+
+  app.use(
+    '/upload',
+    createProxyMiddleware({
+      target: configService.get(EnvKey.UPLOAD_SERVICE),
+      changeOrigin: true,
+    }),
+  );
+
+  app.use(
+    '/catalogue',
+    createProxyMiddleware({
+      target: configService.get(EnvKey.CATALOGUE_SERVICE),
+      xfwd: true,
+      pathRewrite: {
+        '^/catalogue': '',
+      },
+      secure: false,
+      // forward: '',
+    }),
+  );
+
+  app.use(
+    '/buy',
+    createProxyMiddleware({
+      target: configService.get(EnvKey.BUY_SERVICE),
+      changeOrigin: true,
+    }),
+  );
+
+  app.use(
+    '/picture',
+    createProxyMiddleware({
+      target: configService.get(EnvKey.CLOUD_FUNCTION),
+      changeOrigin: true,
+      pathRewrite: {
+        '^/picture': '',
+      },
+    }),
+  );
 
   await app.listen(configService.get(EnvKey.PORT));
 }
